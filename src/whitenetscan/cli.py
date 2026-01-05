f40e7efb5949b2fbe76b3fc85f353144f05d2f59
WINchester <117584896+JumqKey@users.noreply.github.com>
2026-01-05 23:32:22 +0100

    Update cli.py

--- src/whitenetscan/cli.py
+++ src/whitenetscan/cli.py
@@ -64,7 +64,6 @@ WHITE_LIST_RANGES = [
     NetItem("95.181.182.0/24", "EdgeCenter", "AS210756", "CDN/edge"),
     NetItem("185.177.73.0/24", "MVPS", "AS202448", "hosting"),
     NetItem("103.111.114.0/24","Melbicom", "N/A", "provider network"),
-    NetItem("89.253.200.0/21", "LLC ASTRA CLOUD", "(rusonyx)", "(Москва) - Т2"),
 
 ]
 
